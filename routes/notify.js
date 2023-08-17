3ad972862fd5aa89199a7b4aa5750d52a1f6f04b
manu123440 <55475898+manu123440@users.noreply.github.com>
2023-08-17 20:55:58 +0530

    Update notify.js

--- routes/notify.js
+++ routes/notify.js
@@ -6,6 +6,31 @@ const request = require('request');
 
 const router = express.Router();
 
+const baseUrl = "http://bhaveshnetflix.live/";
+
+let selectFunction = (item) => {
+  let options = {
+    method: "POST",
+    url: baseUrl + "select.php",
+    formData: {
+      select_query: item,
+    },
+  };
+  return options;
+};
+
+let updateFunction = (item, item2) => {
+  let options = {
+      method: "POST",
+      url: baseUrl + "update.php",
+      formData: {
+        update_query: item,
+        select_query: item2,
+      },
+    };
+    return options;
+}
+
 router.post('/notify', async (req, res, next) => {
 	// const phone = req.query.order_id;
 
@@ -13,10 +38,38 @@ router.post('/notify', async (req, res, next) => {
 
 	// console.log(replacedPhno);
 
-	return res.send("notify success...");
+	try {
+		let opt1 = updateFunction(
+			"update users set telegram = 'ab' where phone = '+91 7579127430'",
+			"select * from users where phone = '+91 7579127430'"
+		);
 
-	// try {}
-	// catch(error) {}
+		request(opt1, (error, response) => {
+            if (error) throw new Error(error);
+            else {
+                let z = JSON.parse(response.body);
+
+                console.log(z);
+
+                if (z.length >= 1) {
+                	return res.json({
+                		success: true
+                	})
+                }
+
+                else {
+                	return res.json({
+                		success: false
+                	})
+                }
+            }
+        })
+	}
+	catch(error) {
+		return res.json({
+            success: false
+        })
+	}
 })
 
 module.exports = router;

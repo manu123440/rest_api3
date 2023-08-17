03c382f2c358517e6a03417e41407ccb9aceb0f5
manu123440 <55475898+manu123440@users.noreply.github.com>
2023-08-17 18:59:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const checkSubRoute = require('./routes/checkSub');
 const paymentRoute = require('./routes/payment');
 const getVideoRoute = require('./routes/getVideo');
 const getPlansRoute = require('./routes/plans');
+const getSuccessRoute = require('./routes/success');
+const notifyRoute = require('./routes/notify');
+const cancelRoute = require('./routes/cancel');
 
 const app = express();
 
@@ -27,6 +30,9 @@ app.use('/v1', checkSubRoute);
 app.use('/v1', paymentRoute);
 app.use('/v1', getVideoRoute);
 app.use('/v1', getPlansRoute);
+app.use('/v1', getSuccessRoute);
+app.use('/v1', notifyRoute);
+app.use('/v1', cancelRoute);
 
 app.get("/v1/success", (req, res, next) => {
 	return res.send("Success");
